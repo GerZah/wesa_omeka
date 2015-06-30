29437df86db7b36c1f44fa7cc7d4ea5eb39da860
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-06-30 19:10:45 +0200

    various "real world" hard-coded element dependencies

--- plugins/ConditionalElements/ConditionalElementsPlugin.php
+++ plugins/ConditionalElements/ConditionalElementsPlugin.php
@@ -33,14 +33,26 @@ class ConditionalElementsPlugin extends Omeka_Plugin_AbstractPlugin {
 			// An array of dependencies:
 			// Each dependency is represented by a "dependent", a "term", and a "dependee".
 			// ... meaning: If and only if the "dependent"'s value equals the "term", the "dependee" will be visible.
-			 // Example: Only if element 54 contains "- Anderer -", field 60 will become visible.
+			 // Example: Only if element 53 contains "- Anderes Land -", field 59 will become visible.
 			echo <<<EOT
 <script>
-	var dependencies=[
-		["54", "- Anderer -", "60"],
-		["61", "Private Organisation", "62"],
-		["61", "Öffentliche Organisation", "63"],
-	];
+var dependencies=[
+["53", "– Anderes Land –", "59"],
+["54", "– Anderer Ort –", "60"],
+["56", "– Anderer Fundort –", "70"],
+["55", "Bildquelle", "62"], // Bildinhalt
+["62", "– Anderer Bildinhalt –", "63"],
+["55", "Bildquelle", "64"], // Bildart
+["64", "– Andere Bildart –", "65"],
+["55", "Textquelle", "58"],
+["58", "– Andere Textquelle –", "61"],
+["55", "Sachquelle", "66"],
+["66", "– Andere Sachquelle –", "67"],
+["55", "Immaterielle Quelle", "68"],
+["68", "– Andere Immaterielle Quelle –", "69"],
+["57", "– Anderer Beruf –", "71"],
+["72", "– Andere Funktion –", "73"],
+];
 </script>
 
 EOT;
