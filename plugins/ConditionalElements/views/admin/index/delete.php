e7bc35813fedafba57a6857b7d044804d555bfb0
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-08-31 19:41:46 +0200

    delete.php – i18n

--- plugins/ConditionalElements/views/admin/index/delete.php
+++ plugins/ConditionalElements/views/admin/index/delete.php
@@ -1,13 +1,14 @@
 <?php
-$pageTitle = __('Delete dependency');
+$pageTitle = __('Delete Dependency');
 echo head(array('title'=>$pageTitle));
 echo flash();
 ?>
+
 <form method="post" action="<?php echo url('conditional-elements/index'); ?>">
   <section class="seven columns alpha">
     <fieldset class="bulk-metadata-editor-fieldset" id='bulk-metadata-editor-items-set' style="border: 1px solid black; padding:15px; margin:10px;">
       <div class="field">
-        <h2>You have successfully deleted the dependency</h2>
+        <h2><?php echo __("You have successfully deleted the dependency."); ?></h2>
       </div>
     </fieldset>
   </section>
