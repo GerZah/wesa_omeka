92b56dd3f20025e2bde4982f78230aa998cb1984
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-08-31 18:08:47 +0200

    Confirm delete: Tiny bugfix (mine!) / Error conditions / text formatting / i18n

--- plugins/ConditionalElements/views/admin/index/confirm.php
+++ plugins/ConditionalElements/views/admin/index/confirm.php
@@ -1,50 +1,65 @@
 <?php
-$pageTitle = __('Confirm delete');
+$pageTitle = __('Confirm Delete');
 echo head(array('title'=>$pageTitle));
 echo flash();
 ?>
 <?php
-if(!isset($_SESSION))
-{
-  session_start();
-}
-$_SESSION['conditional_elements_delete_dependent'] = $_GET['dependent_id'];
+if (!isset($_SESSION)) { session_start(); }
+if (isset($_GET['dependent_id'])) { $_SESSION['conditional_elements_delete_dependent'] = $_GET['dependent_id']; }
 ?>
 <form method="post" action="" >
   <div class="field">
     <?php
-    echo $this->formLabel('confirm', __('Are you sure you wish to delete this dependency?'));
-    $dependent_id = null;
-    $term = null;
-    $dependent_id = $_GET['dependent_id'];
-
-    $json=get_option('conditional_elements_dependencies');
-    if (!$json) { $json="null"; }
-    $dependencies = json_decode($json,true);
-    foreach($dependencies as $dependency) {
-      if ($dependency[2] == $dependent_id) {
-				$dependee_id = $dependency[0];
-				$term = $dependency[1];
+    if (isset($_GET['dependent_id'])) {
+      $dependent_id = intval($_GET['dependent_id']);
+      $dependee_id = null;
+      $term = null;
+  
+      $json=get_option('conditional_elements_dependencies');
+      if (!$json) { $json="null"; }
+      $dependencies = json_decode($json,true);
+			if ($dependencies) {
+	      foreach($dependencies as $dependency) {
+	        if ($dependency[2] == $dependent_id) {
+	          $dependee_id = $dependency[0];
+	          $term = $dependency[1];
+	        }
+	      }
 			}
-    }
+  
+			if ( ($dependent_id) and ($term) and ($dependee_id) ) {
+	      $db = get_db();
+	      $select = "SELECT id, name FROM $db->Element WHERE id in ($dependent_id, $dependee_id)";
+	      $results = $db->fetchAll($select);
 
-    $ids = array();
-    $ids[0]=$dependee_id;
-    $ids[1]=$dependent_id;
-    $ids=array_unique($ids);
-    $ids_verb = implode(",",$ids);
-    $db = get_db();
-    $select = "SELECT id, name FROM $db->Element WHERE id in ($ids_verb)";
-    $results = $db->fetchAll($select);
-    $data = array();
-    foreach($results as $result) {
-      $data[$result['id']] = $result['name'];
+				$dependee = $dependent = null;
+	      foreach($results as $result) {
+					switch ($result['id']) {
+						case $dependent_id : $dependent = $result['name']; break;
+						case $dependee_id : $dependee = $result['name']; break;
+					}
+	      }
+
+				if ( ($dependent) and ($dependee) ) {
+    ?>
+		<h3><?php echo __('Are you sure you wish to delete this dependency?'); ?></h3>
+		<table>
+			<tbody>
+				<tr><th><?php echo __("Dependee"); ?>:</th><td><?php echo $dependee; ?></td></tr>
+				<tr><th><?php echo __("Term"); ?>:</th><td><?php echo $term; ?></td></tr>
+				<tr><th><?php echo __("Dependent"); ?>:</th><td><?php echo $dependent; ?></td></tr>
+			</tbody>
+		</table>
+    <a href="<?php echo html_escape(url('conditional-elements/index/delete')); ?>" class="button remove flr mrr4"><?php echo __("Yes"); ?></a>
+    <a href="<?php echo html_escape(url('conditional-elements/index')); ?>" class="button buttonGreen cancel flr"><?php echo __("No"); ?></a>
+    <?php
+				}
+				else { echo "<h3>".__('Dependency element names of dependency that should be deleted not found.')."</h3>\n"; }
+			}
+			else { echo "<h3>".__('Dependency that should be deleted not found.')."</h3>\n"; }
     }
-    echo $this->formLabel('dependeeName', $data[$dependee_id]);
-    echo $this->formLabel('termName', $term);
-    echo $this->formLabel('dependentName', $data[$result['id']]); ?>
-    <a href="<?php echo html_escape(url('conditional-elements/index/delete')); ?>" class="button remove flr mrr4">Yes</a>
-    <a href="<?php echo html_escape(url('conditional-elements/index')); ?>" class="button buttonGreen cancel flr">No</a>
+		else { echo "<h3>".__('No dependency given that could be deleted.')."</h3>\n"; }
+    ?>
   </div>
 </form>
 <?php echo foot(); ?>
