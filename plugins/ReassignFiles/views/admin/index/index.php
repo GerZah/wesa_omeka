403fe6bb0eb2373a0c1bbd43421ef21db31e4ef1
githanjalli elangovan <anjalli@mail.uni-paderborn.de>
2015-09-18 14:01:41 +0200

    created UI for reassign files

--- plugins/ReassignFiles/views/admin/index/index.php
+++ plugins/ReassignFiles/views/admin/index/index.php
@@ -1,14 +1,37 @@
 <?php
-    queue_js_file('items');
-    queue_js_file('tabs');
-    queue_css_file('reassignfiles');
-    echo head(array('title' => __('reassignfiles'), 'bodyclass' => 'reassignfiles'));
-  ?>
-
-<?php echo flash(); ?>
-<p>
-<?php
-echo __("View the files that are currently assigned to items");
+queue_js_file('items');
+queue_js_file('tabs');
+queue_css_file('reassignfiles');
+echo head(array('title' => __('Reassign Files to items'), 'bodyclass' => 'reassignfiles'));
 ?>
-</p>
+<?php echo flash(); ?>
+<div class="drawer-contents">
+  <fieldset class="bulk-metadata-editor-fieldset" id='bulk-metadata-editor-items-set' style="border: 1px solid black; padding:15px; margin:10px;">
+    <h2>Step 1: Select Items to Edit </h2>
+    <div class="field">
+      <p>Edit items from the following collection:</p>
+    </div>
+    <div class="inputs three columns omega">
+      <?php echo $this->formSelect('reassignFiles-item',null,array('multiple' => false), null); ?>
+    </div>
+  </fieldset>
+  <fieldset class="bulk-metadata-editor-fieldset" id='bulk-metadata-editor-fields-set' style="border: 1px solid black; padding:15px; margin:10px;">
+    <h2>Step 2: Select Files to Assign </h2>
+    <div class="inputs four columns omega">
+      <?php
+      $fileNames = array();
+      $db = get_db();
+      $select = "SELECT original_filename from $db->File order by original_filename";
+      $files = $db->fetchAll($select);
+      foreach ($files as $file) {
+        $fileNames[$file['original_filename']] = $file['original_filename'];
+      }
+      echo $this->formSelect('reassignFiles-files[]', null, array('multiple' => true, 'size' => 10, 'style' => 'width: 300px;'), $fileNames);
+      ?>
+    </div>
+  </fieldset>
+  <div class="field">
+    <button type="submit" name="reassign-button">Reassign Files</button>
+  </div>
+</div>
 <?php echo foot();
--- plugins/ReassignFiles/views/admin/index/reassignfileslist.php
+++ plugins/ReassignFiles/views/admin/index/reassignfileslist.php
@@ -1,87 +1,14 @@
-<script type="text/javascript">
-            function reassignFilesSelectAllCheckboxes(checked) {
-                jQuery('#reassignFiles-file-checkboxes tr:visible input').each(function() {
-                    this.checked = checked;
-                });
-                jQuery('#reassignFiles-file-checkboxes').trigger('reassignFiles-all-toggled');
-            }
-
-            function reassignFilesFilterFiles() {
-                var filter = jQuery.trim(jQuery('#reassignFiles-file-filter').val().toLowerCase());
-                var someHidden = false;
-                jQuery('#reassignFiles-file-checkboxes input').each(function() {
-                    var v = jQuery(this);
-                    if (filter != '') {
-                        if (v.val().toLowerCase().indexOf(filter) != -1) {
-                            v.parent().parent().show();
-                        } else {
-                            v.parent().parent().hide();
-                            someHidden = true;
-                        }
-                    } else {
-                        v.parent().parent().show();
-                    }
-                });
-                jQuery('#reassignFiles-show-all').toggle(someHidden);
-            }
-
-            function reassignFilesNoEnter(e) {
-                var e  = (e) ? e : ((event) ? event : null);
-                var node = (e.target) ? e.target : ((e.srcElement) ? e.srcElement : null);
-                if ((e.keyCode == 13) && (node.type=="text")) {return false;}
-            }
-
-            jQuery(document).ready(function () {
-                jQuery('#reassignFiles-select-all').click(function () {
-                    reassignFilesSelectAllCheckboxes(this.checked);
-                });
-
-                jQuery('#reassignFiles-show-all').click(function (event) {
-                    event.preventDefault();
-                    jQuery('#reassignFiles-file-filter').val('');
-                    reassignFilesFilterFiles();
-                });
-
-                jQuery('#reassignFiles-file-filter').keyup(function () {
-                    reassignFilesFilterFiles();
-                }).keypress(reassignFilesNoEnter);
-
-                jQuery('.reassignFiles-js').show();
-                jQuery('#reassignFiles-show-all').hide();
-            });
-        </script>
-      <div class="add-new"><?php echo __('Add from Existing Files'); ?></div>
-                <p class="reassignFiles-js" style="display:none;">
-                    <?php echo __('Filter files by name:'); ?>
-                    <input type="text" id="reassignFiles-file-filter">
-                    <button type="button" id="reassignFiles-show-all" class="blue"><?php echo __('Show All'); ?></button>
-                </p>
-                <div class="drawer-contents">
-                <table>
-                    <colgroup>
-                        <col style="width: 2em">
-                        <col>
-                    </colgroup>
-                    <thead>
-                        <tr>
-                            <th><input type="checkbox" id="reassignFiles-select-all" class="reassignFiles-js" style="display:none"></th>
-                            <th><?php echo __('File Name'); ?></th>
-                        </tr>
-                    </thead>
-                    <tbody id="reassignFiles-file-checkboxes">
-                      <?php
-                       $itemId = metadata('item', 'id');
-                       $fileNames = array();
-                       $db = get_db();
-                       $select = "SELECT original_filename from $db->File where item_id !=$itemId order by original_filename";
-                       $files = $db->fetchAll($select);
-                       foreach ($files as $file) {
-                        $fileNames[$file['original_filename']] = $file['original_filename'];
-                       }
-                      ?>
-                    <?php foreach ($fileNames as $fileName): ?>
-                        <tr><td><input type="checkbox" name="reassignFiles-files[]" value="<?php echo html_escape($fileName); ?>"/></td><td><?php echo html_escape($fileName); ?></td></tr>
-                    <?php endforeach; ?>
-                    </tbody>
-                </table>
-        <a class="add green button"><?php echo __('Assign'); ?></a> </div>
+<div class="add-new"><?php echo __('Select the files to reassign to the item: %s',metadata('item', array('Dublin Core', 'Title'))); ?></div>
+<div class="drawer-contents">
+  <?php
+  $itemId = metadata('item', 'id');
+  $fileNames = array();
+  $db = get_db();
+  $select = "SELECT original_filename from $db->File where item_id !=$itemId order by original_filename";
+  $files = $db->fetchAll($select);
+  foreach ($files as $file) {
+    $fileNames[$file['original_filename']] = $file['original_filename'];
+  }
+  ?>
+  <?php echo get_view()->formSelect('reassignFiles-files[]', null, array('multiple' => true, 'size' => 10, 'style' => 'width: 500px;'), $fileNames); ?>
+</div>
