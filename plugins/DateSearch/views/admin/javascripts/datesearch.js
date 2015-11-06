00ddb6bedd81eb230eeeb99b4db0b463f4d1235b
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-11-06 12:02:09 +0100

    First working prototype to add / edit dates into/in text areas (yet unspecific, i.e. Gregorian without a prefix)

--- plugins/DateSearch/views/admin/javascripts/datesearch.js
+++ plugins/DateSearch/views/admin/javascripts/datesearch.js
@@ -6,52 +6,45 @@ jQuery(document).bind("omeka:elementformload", function() {
     .css("position", "relative")
     .append("<span id='dateSearchWrapper'>"+
               "<div class='dateSearchButtons field'>"+
-                // "<button id='dateSearchFooBtn'>foo</button>"+
                 "<button id='dateSearchDBtn'>D</button>"+
+                "<input id='dateSearchEdit' class='dateSearchHiddenEdit'>"+
                 "</div>"+
               "</span>");
 
   var currentTextArea = false;
   $("textarea").focus(function(e) { currentTextArea = $(this); })
 
-  /*
-  $("#dateSearchFooBtn").click(function(e) {
-    e.preventDefault();
-
-    if (currentTextArea) {
-      // https://github.com/timdown/rangyinputs
-      currentTextArea.replaceSelectedText("foo");
+  $("#dateSearchEdit").calendarsPicker({
+    showOnFocus: false,
+    firstDay: 1,
+		yearRange: 'any',
+    // rangeSelect: false, // true
+    // dateFormat: "yyyy-mm-dd",
+    // calendar: $.calendars.instance('gregorian', elTypesLocale),
+    onClose: function(dates) {
+      // console.log('Closed with date(s): ' + dates);
+      if (currentTextArea) {
+         var newDates = $("#dateSearchEdit").val();
+         if (newDates) { currentTextArea.replaceSelectedText(newDates); }
+         $("#dateSearchEdit").val("");
+       }
     }
   });
-  */
 
-  $("#dateSearchDBtn")
-  .calendarsPicker(
-    { showOnFocus: false,
-      firstDay: 1,
-			yearRange: 'any',
-      rangeSelect: false, // true
-      // dateFormat: "dd.mm.yy",
-      calendar: $.calendars.instance('gregorian', elTypesLocale),
-      onClose: function(dates) {
-        console.log('Closed with date(s): ' + dates);
-        if (currentTextArea) {
-          var newDates = String(dates);
-          if (newDates) { currentTextArea.replaceSelectedText(newDates); }
-        }
-      }
-    }
-  )
-  .click(function(e) {
+  $("#dateSearchDBtn").click(function(e) {
     e.preventDefault();
     if (currentTextArea) {
-      $(this).val(''); // +#+#+# this can't be right - calendar reference required to reset
       var sel = currentTextArea.getSelection();
-      console.log(sel.text);
+      // console.log(sel.text);
       var selText = null;
       if (sel.start != sel.end) { selText = sel.text; }
-      $(this).calendarsPicker({ defaultDate: selText }); // +#+#+# this doesn't work
-      $(this).calendarsPicker("show");
+      $("#dateSearchEdit").val(selText);
+      $("#dateSearchEdit").calendarsPicker("option", {
+        calendar: $.calendars.instance("gregorian", elTypesLocale),
+        dateFormat: "yyyy-mm-dd",
+        rangeSelect: false
+      } );
+      $("#dateSearchEdit").calendarsPicker("show");
     }
     else { alert("First select target text area."); }
   });
