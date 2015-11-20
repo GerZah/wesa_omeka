d48cb1e50a7260872f0c28ad09355615637cbf87
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-11-20 13:17:09 +0100

    WebGLViewer: Bugfix – address multiple iframes correctly, in public view of objects with multiple 3D objects

--- plugins/WebGLViewer/WebGLhelper.js
+++ plugins/WebGLViewer/WebGLhelper.js
@@ -1,8 +1,8 @@
-jQuery('#webGlFrame').on("load", function() {
+jQuery('.webGlFrame').on("load", function(event) {
   var $ = jQuery; // use noConflict version of jQuery as the short $ within this block
 
-  $('#webGlFrame').contents().find( "div#edit" ).css( "width", "0px" );
+  $('.webGlFrame').contents().find( "div#edit" ).css( "width", "0px" );
+  $('.webGlFrame').contents().find( "div#view" ).css( "margin-right", "0px" );
 
-  $('#webGlFrame').contents().find( "div#view" ).css( "margin-right", "0px" );
-  document.getElementById('webGlFrame').contentWindow.resize(); // http://stackoverflow.com/a/22148030
+  document.getElementById(event.currentTarget.id).contentWindow.resize(); // http://stackoverflow.com/a/22148030
 } );
