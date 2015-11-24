890e71c1ba970fa75d4320c326619aee07372286
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-11-24 16:05:05 +0100

    RangeSearch: First working prototype with single / dual triplet number / range entry plus units

--- plugins/RangeSearch/RangeSearchUI.php
+++ plugins/RangeSearch/RangeSearchUI.php
@@ -8,37 +8,89 @@
   queue_js_file('rangesearch');
   queue_css_file('rangesearch');
 
-  $rangeEntry   = __("Range Entry");
   $selectFirst = __("Please select a target text area first.");
+  $selectUnit = __("Please select a unit.");
+  $enterNumber = __("Please enter a number.");
 
   queue_js_string("
     var rangeSearchSelectFirst='$selectFirst';
+    var rangeSearchSelectUnit='$selectUnit';
+    var rangeSearchEnterNumber='$enterNumber';
   ");
+
+  $view = get_view();
+
+  // ------------------------------------------------------
+
+  function editFieldHTML($textField, $view = false) {
+    if (!$view)  {$view = get_view(); }
+    return $view->formInput($textField,
+                            null,
+                            array("type" => "text",
+                                  "class" => "rangeSearchTextField",
+                                  "size" => 4,
+                                  "maxlength" => 10,
+                                )
+                            );
+  }
+
+  // ------------------------------------------------------
 ?>
 
 <div id="range-search-popup" style="overflow: auto; padding: 20px; border-radius: 6px; background: #fff" class="lity-hide">
-  <h2>Foo</h2>
-  <div>
+  <h2><?php echo __("Range Entry"); ?></h2>
+  <p>
   <?php
     $units = array( -1 => __("Select Below") ) + SELF::_fetchUnitArray();
-
-    $view = get_view();
-    echo $view->formSelect('units', -1, array(), $units);
+    $saniUnits = array();
+    foreach($units as $unit) {
+      if ( substr_count($unit, "-") == 2 ) { $saniUnits[] = $unit;}
+    }
+    echo __("Units") . ": ". $view->formSelect('rangeSearchUnits', -1, array(), $saniUnits);
   ?>
-  </div>
+  </p>
   <p>
-    <a href="#" id="range-search-Cancel" class="green button" data-lity-close>
+    <?php
+      $textFields = array("rangeSearch1", "rangeSearch2", "rangeSearch3");
+      $htmlTextFields = array();
+      foreach($textFields as $textField) {
+        $htmlTextFields[] = editFieldHTML($textField, $view);
+      }
+      echo implode(" — ", $htmlTextFields);
+    ?>
+  </p>
+  <p>
+    <?php
+      echo $view->formCheckbox("rangeSearchRange", false, array() );
+      echo " <label for='rangeSearchRange'>".
+            # __("Enter range (not just number).").
+            __("… (Range)").
+            "</label>";
+    ?>
+  </p>
+  <p id="rangeSearchSecondTriple">
+    <?php
+      $textFields = array("rangeSearch4", "rangeSearch5", "rangeSearch6");
+      $htmlTextFields = array();
+      foreach($textFields as $textField) {
+        $htmlTextFields[] = editFieldHTML($textField, $view);
+      }
+      echo implode(" — ", $htmlTextFields);
+    ?>
+  </p>
+  <p style="text-align: center;">
+    <button id="rangeSearchCancel" class="green button" data-lity-close>
       <?php echo __("Cancel"); ?>
-    </a>
-    <a href="#" id="range-search-apply" class="green button" data-lity-close>
+    </button>
+    <button id="rangeSearchApply" class="green button"> <!--  data-lity-close -->
       <?php echo __("Apply"); ?>
-    </a>
+    </button>
   </p>
 </div>
 
 <div id="range-search-controls" style="display:none;">
   <div class='rangeSearchButtons field'>
-    <label><?php echo $rangeEntry; ?>:</label>
+    <label><?php echo __("Range Entry"); ?>:</label>
     <button class='rangeSearchBtn'><?php echo __("Entry"); ?></button>
   </div>
 </div>
--- plugins/RangeSearch/views/admin/javascripts/rangesearch.js
+++ plugins/RangeSearch/views/admin/javascripts/rangesearch.js
@@ -21,25 +21,96 @@ jQuery(document).bind("omeka:elementformload", function() {
   $(".rangeSearchButtons button").click(function(e) {
     e.preventDefault();
 
-    if (currentTextArea) {
+    if (!currentTextArea) { alert(rangeSearchSelectFirst); return; }
 
-      var sel = currentTextArea.getSelection();
-      var selText = "";
-      if (sel.start != sel.end) { selText = sel.text; }
+    var sel = currentTextArea.getSelection();
+    var selText = "";
+    if (sel.start != sel.end) { selText = sel.text; }
 
-      lightbox("#range-search-popup");
-      console.log(selText);
-
-    }
-    else { alert(rangeSearchSelectFirst); }
+    console.log(selText);
+    lightbox("#range-search-popup");
+    showHideSecondTriple();
   });
 
   // --------------------------------------------------------
 
-  $("#range-search-apply").click(function () {
-    if (currentTextArea) {
-      currentTextArea.replaceSelectedText("FOO");
+  $("#range-search-popup button").click(function(e) { e.preventDefault(); });
+
+  // --------------------------------------------------------
+
+  function isRange() {
+    return $("#rangeSearchRange").is(":checked");
+  }
+
+  // -------------------
+
+  function showHideSecondTriple() {
+    if (isRange()) {
+      $("#rangeSearchSecondTriple").slideDown("fast");
     }
+    else {
+      $("#rangeSearchSecondTriple").slideUp("fast");
+    }
+  }
+
+  // -------------------
+
+  $("#rangeSearchRange").change(function() {
+    showHideSecondTriple();
+  });
+
+  // --------------------------------------------------------
+
+  function checkTextfields(textFields) {
+    var textField;
+    for(textField of textFields) {
+      if (!$(textField).val().match(/\d+/)) {
+        alert(rangeSearchEnterNumber);
+        $(textField).focus();
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // -------------------
+
+  $("#rangeSearchApply").click(function () {
+    if (!currentTextArea) { alert(rangeSearchSelectFirst); return; }
+
+    if ($("#rangeSearchUnits").val() == -1) {
+      alert(rangeSearchSelectUnit);
+      $("#rangeSearchUnits").focus();
+      return;
+    }
+
+    var textFields = ["#rangeSearch1", "#rangeSearch2", "#rangeSearch3"];
+    if (!checkTextfields(textFields)) { return; }
+
+    var range = isRange();
+
+    if (range) {
+      var textFields = ["#rangeSearch4", "#rangeSearch5", "#rangeSearch6"];
+      if (!checkTextfields(textFields)) { return; }
+    }
+
+    var units = $('#rangeSearchUnits option:selected').text();
+    units = units.split("-");
+
+    var result="";
+    result += $("#rangeSearch1").val() + units[0] + "-" +
+              $("#rangeSearch2").val() + units[1] + "-" +
+              $("#rangeSearch3").val() + units[2];
+
+    if (range) {
+      result += " - " +
+                $("#rangeSearch4").val() + units[0] + "-" +
+                $("#rangeSearch5").val() + units[1] + "-" +
+                $("#rangeSearch6").val() + units[2];
+    }
+
+    currentTextArea.replaceSelectedText(result);
+    lightbox.close();
   });
 
   // --------------------------------------------------------
