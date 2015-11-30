33fcc6e05f3a7eee5f41d85a7d9d5e01f148da96
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-11-30 19:40:41 +0100

    MeasurementsSearch: Initial RegEx processing, but no storage yet

--- plugins/MeasurementSearch/MeasurementSearchPlugin.php
+++ plugins/MeasurementSearch/MeasurementSearchPlugin.php
@@ -19,8 +19,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		'uninstall', # delete table
 		'config_form', # prepare and display configuration form
 		'config', # store config settings in the database
-		# 'after_save_item', # preprocess saved item for ranges
-		# 'after_delete_item', # delete deleted item's preprocessed ranges
+		'after_save_item', # preprocess saved item for ranges
+		'after_delete_item', # delete deleted item's preprocessed ranges
 		# 'admin_items_search', # add a time search field to the advanced search panel in admin
 		# 'public_items_search', # add a time search field to the advanced search panel in public
 		# 'admin_items_show_sidebar', # Debug output of stored numbers/ranges in item's sidebar (if activated)
@@ -171,27 +171,21 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		return ( $arr ? implode("\n", $arr) : "" );
 	}
 
-//	/**
-//	 * Transform plausible entries from units array for use in RegEx
-//	 */
-//	private function _decodeUnitsForRegEx() {
-//		$result = array();
-//
-//		$arr = SELF::_fetchUnitArray();
-//		if ($arr) {
-//			foreach($arr as $unit) {
-//				if ( substr_count($unit, "-") == 2 ) { // e.h. "RT-Gr-d"
-//					$units = explode("-", $unit);
-//					foreach(array_keys($units) as $idx) { $units[$idx] = preg_quote(trim($units[$idx])); }
-//					if ( $units[0] && $units[1] && $units[2] ) {
-//						$result[$unit] = $units;
-//					}
-//				}
-//			}
-//		}
-//
-//		return $result;
-//	}
+  /**
+  * Transform plausible entries from units array for use in RegEx
+  */
+  private function _decodeUnitsForRegEx() {
+    $result = array();
+
+    $arr = SELF::_fetchUnitArray();
+    if ($arr) {
+      foreach($arr as $unit) {
+        if ( $sanUnit = preg_quote(trim($unit)) ) { $result[] = $sanUnit; }
+      }
+    }
+
+    return $result;
+  }
 
 	/**
 	 * Encode content of textarea on config page to be stored as a JSON array in DB option
@@ -296,11 +290,141 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 	/**
 	 * Pre-process one item's textual data and store measurements in MeasurementSearchValues table
 	 */
-	private function _preProcessItem($item_id) {
-		$db = get_db();
+  private function _preProcessItem($item_id) {
+    $db = get_db();
 
-		if ($item_id) {
-    }
+    if ($item_id) {
+      $sql = "delete from `$db->MeasurementSearchValues` where item_id=$item_id";
+      $db->query($sql);
+
+      $text = false;
+
+      $searchAllFields = (int)(boolean) get_option('measurement_search_search_all_fields');
+
+      if ($searchAllFields) {
+        $text = $db->fetchOne("select text from `$db->SearchTexts` where record_type='Item' and record_id=$item_id");
+        $text = ( $text ? $text : "" );
+
+        $text .= SELF::_relationshipCommentText($item_id);
+        $text = ( $text ? $text : false );
+      } # if ($searchAllFields)
+
+      else { # !$searchAllFields
+
+        $limitFields = get_option('measurement_search_limit_fields');
+        $limitFields = ( $limitFields ? json_decode($limitFields) : array() );
+
+        $elementIds=array();
+        if (is_array($limitFields)) {
+          foreach($limitFields as $limitField) {
+            $limitField = intval($limitField);
+            if ($limitField) { $elementIds[] = $limitField; }
+          }
+          sort($elementIds);
+        }
+
+        if ($elementIds) {
+          $elementIds = "(" . implode(",", $elementIds) . ")";
+
+          $elementTexts = $db -> fetchAll("select text from `$db->ElementTexts`".
+                                          " where record_id=$item_id".
+                                          " and element_id in $elementIds");
+          if ($elementTexts) {
+            $text = "";
+            foreach($elementTexts as $elementText) { $text .= " " . $elementText["text"]; }
+          } # if ($elementTexts)
+        } # if ($elementIds)
+
+        $searchRelComments = (int)(boolean) get_option('measurement_search_search_rel_comments');
+
+        if ($searchRelComments) {
+          $text = ( $text ? $text : "" );
+          $text .= SELF::_relationshipCommentText($item_id);
+          $text = ( $text ? $text : false );
+        }
+
+      }  # !$searchAllFields
+
+      if ($text !== false) {
+
+        //  $cookedRanges = SELF::_processRangeText($text);
+        //  # echo "<pre>"; print_r($cookedRanges); die("</pre>");
+        //
+        //  if ($cookedRanges) {
+        //
+        //    $values = array();
+        //    foreach($cookedRanges as $cookedRange) {
+        //      SELF::_swapIfNecessary($cookedRange[0], $cookedRange[1]);
+        //      $values[]='('.$item_id.',"'.$cookedRange[0].'","'.$cookedRange[1].'","'.$cookedRange[2].'")';
+        //    }
+        //    $values = implode(", ", $values);
+        //
+        //    $sql = "insert into `$db->RangeSearchValues` (item_id, fromnum, tonum, unit) values $values";
+        //    $db->query($sql);
+        //    # die($sql);
+        //
+        //  } # if ($cookedDates)
+
+        $cookedMeasurements = SELF::_processMeasurementText($text);
+        # +#+#+#
+        // echo "<pre>" . print_r($cookedMeasurements,true) . "</pre>";
+        // die();
+
+      } # if ($text)
+    } # if ($item_id)
+  } #  _preProcessItem()
+
+  /**
+	 * Main regex processing to extract measurements, to be able to store them later
+	 */
+	private function _processMeasurementText($text) {
+		$regEx = SELF::_constructRegEx();
+		# echo "<pre>$text\n" . print_r($regEx,true) . "</pre>";
+		foreach($regEx as $key => $val) { $$key = $val; }
+
+		$allCount = preg_match_all( "($fullRegEx)i", $text, $allMatches);
+		# echo "<pre>Count: $allCount\n" . print_r($allMatches,true) . "</pre>";
+
+		$cookedMeasuerements = array();
+		foreach(array_keys($allMatches[0]) as $singleIdx) {
+
+      # echo "<pre>" . print_r($allMatches[0][$singleIdx],true) . "</pre>";
+
+      $cookedMeasuerement = array();
+      for($i=1 ; $i<=4 ; $i++) {
+        $cookedMeasuerement[] = $allMatches[$i][$singleIdx];
+      }
+      $cookedMeasuerements[] = $cookedMeasuerement;
+
+		}
+		# echo "<pre>" . print_r($cookedMeasuerements,true) . "</pre>"; die();
+
+		return $cookedMeasuerements;
+	}
+
+  /**
+	 * Create the necessary regEx expressions to deal with 123x456x789 measurements
+	 */
+	private function _constructRegEx() {
+
+    $oneNumber = "\d{1,".MEASUREMENTSEARCH_NUM_MAXLEN."}";
+    $mainTriple = "($oneNumber)x($oneNumber)x($oneNumber)";
+
+    $fullRegEx = $mainTriple;
+
+    $DBunits = SELF::_decodeUnitsForRegEx();
+    if ($DBunits) { $fullRegEx .= "\s(" . implode("|", $DBunits) . ")"; }
+
+    $result = array(
+      "oneNumber" => $oneNumber,
+      "mainTriple" => $mainTriple,
+      "fullRegEx" => $fullRegEx,
+    );
+
+    // echo "<pre>\n\n\n\n\n</pre>";
+    // echo "<pre>" . print_r($result, true) . "</pre>";
+
+    return $result;
   }
 
 } # class
