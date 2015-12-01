19cd2820438ac70f5c66a36e488de512db588fad
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2015-12-01 11:56:48 +0100

    MeasurementSearch: Work in progress – database storage complete, search under construction

--- plugins/MeasurementSearch/MeasurementSearchPlugin.php
+++ plugins/MeasurementSearch/MeasurementSearchPlugin.php
@@ -19,14 +19,15 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		'uninstall', # delete table
 		'config_form', # prepare and display configuration form
 		'config', # store config settings in the database
-		'after_save_item', # preprocess saved item for ranges
-		'after_delete_item', # delete deleted item's preprocessed ranges
-		# 'admin_items_search', # add a time search field to the advanced search panel in admin
-		# 'public_items_search', # add a time search field to the advanced search panel in public
-		# 'admin_items_show_sidebar', # Debug output of stored numbers/ranges in item's sidebar (if activated)
-		# 'items_browse_sql', # filter for a range after search page submission.
+		'after_save_item', # preprocess saved item for measurements
+		'after_delete_item', # delete deleted item's preprocessed measurements
+		'admin_items_search', # add a time search field to the advanced search panel in admin
+		'public_items_search', # add a time search field to the advanced search panel in public
+		'admin_items_show_sidebar', # Debug output of stored measurements in item's sidebar (if activated)
+		'items_browse_sql', # filter for a measurement after search page submission.
 	);
 
+  # ----------------------------------------------------------------------------
   protected $_options = array(
 		'measurement_search_units' => '',
 		'measurement_search_search_all_fields' => 1,
@@ -35,12 +36,15 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		'measurement_search_debug_output' => 0,
 	);
 
+  # ----------------------------------------------------------------------------
   /**
 	 * Add the translations.
 	 */
 	public function hookInitialize() {
 		add_translation_source(dirname(__FILE__) . '/languages');
 	}
+
+  # ----------------------------------------------------------------------------
 
   /**
 	 * Install the plugin.
@@ -70,6 +74,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		# SELF::_batchProcessExistingItems();
 	}
 
+  # ----------------------------------------------------------------------------
+
 	/**
 	 * Uninstall the plugin.
 	 */
@@ -82,6 +88,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 
 		SELF::_uninstallOptions();
 	}
+
+  # ----------------------------------------------------------------------------
 
   /**
 	 * Display the plugin configuration form.
@@ -111,6 +119,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 
 		# SELF::_constructRegEx(); // +#+#+# DEBUG
 	}
+
+  # ----------------------------------------------------------------------------
 
   /**
 	 * Handle the plugin configuration form.
@@ -154,6 +164,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		# echo "<pre>"; print_r($_POST); echo "</pre>"; die();
 	}
 
+  # ----------------------------------------------------------------------------
+
   /**
 	 * Fetch JSON array from DB option as a PHP array
 	 */
@@ -163,6 +175,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		return json_decode($json);
 	}
 
+  # ----------------------------------------------------------------------------
+
 	/**
 	 * Transform unit array to be edited in textarea on config page
 	 */
@@ -170,6 +184,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		$arr = SELF::_fetchUnitArray();
 		return ( $arr ? implode("\n", $arr) : "" );
 	}
+
+  # ----------------------------------------------------------------------------
 
   /**
   * Transform plausible entries from units array for use in RegEx
@@ -186,6 +202,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 
     return $result;
   }
+
+  # ----------------------------------------------------------------------------
 
 	/**
 	 * Encode content of textarea on config page to be stored as a JSON array in DB option
@@ -206,6 +224,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		return json_encode($nonEmptyLines);
 	}
 
+  # ----------------------------------------------------------------------------
+
 	/**
 	 * Preprocess ALL existing items which could be rather EVIL in huge installations
 	 */
@@ -216,8 +236,10 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		foreach($items as $item) { SELF::_preProcessItem($item["id"]); }
 	}
 
+  # ----------------------------------------------------------------------------
+
 	/**
-	 * Preprocess numbers after saving an item add/edit form.
+	 * Preprocess measurements after saving an item add/edit form.
 	 *
 	 * @param array $args
 	 */
@@ -233,8 +255,10 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 
 	} # hookAfterSaveItem()
 
+  # ----------------------------------------------------------------------------
+
 	/**
-	 * Delete pre-processed numbers after an item has been deleted
+	 * Delete pre-processed measurements after an item has been deleted
 	 *
 	 * @param array $args
 	 */
@@ -251,6 +275,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 			# echo "<pre>After Delete Item - ID: $item_id\nSQL: $sql\n"; print_r($args); die("</pre>");
 	} # hookAfterDeleteItem()
 
+  # ----------------------------------------------------------------------------
+
 	/**
 	 * Determine if Item Relations is installed, and if it's patched to feature relationship comments
 	 */
@@ -264,6 +290,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 
 		return $withRelComments;
 	}
+
+  # ----------------------------------------------------------------------------
 
 	/**
 	 * Get an item's relationship comment text
@@ -286,6 +314,8 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 
 		return $text;
 	}
+
+  # ----------------------------------------------------------------------------
 
 	/**
 	 * Pre-process one item's textual data and store measurements in MeasurementSearchValues table
@@ -347,32 +377,37 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 
       if ($text !== false) {
 
-        //  $cookedRanges = SELF::_processRangeText($text);
-        //  # echo "<pre>"; print_r($cookedRanges); die("</pre>");
-        //
-        //  if ($cookedRanges) {
-        //
-        //    $values = array();
-        //    foreach($cookedRanges as $cookedRange) {
-        //      SELF::_swapIfNecessary($cookedRange[0], $cookedRange[1]);
-        //      $values[]='('.$item_id.',"'.$cookedRange[0].'","'.$cookedRange[1].'","'.$cookedRange[2].'")';
-        //    }
-        //    $values = implode(", ", $values);
-        //
-        //    $sql = "insert into `$db->RangeSearchValues` (item_id, fromnum, tonum, unit) values $values";
-        //    $db->query($sql);
-        //    # die($sql);
-        //
-        //  } # if ($cookedDates)
-
         $cookedMeasurements = SELF::_processMeasurementText($text);
-        # +#+#+#
         // echo "<pre>" . print_r($cookedMeasurements,true) . "</pre>";
         // die();
 
+        if ($cookedMeasurements) {
+
+          $values = array();
+          foreach($cookedMeasurements as $cookedMeasurement) {
+            $values[] =
+            '('.
+              $item_id . ',"' .
+              str_pad($cookedMeasurement[0], MEASUREMENTSEARCH_NUM_MAXLEN, "0", STR_PAD_LEFT) . '","' .
+              str_pad($cookedMeasurement[1], MEASUREMENTSEARCH_NUM_MAXLEN, "0", STR_PAD_LEFT) . '","' .
+              str_pad($cookedMeasurement[2], MEASUREMENTSEARCH_NUM_MAXLEN, "0", STR_PAD_LEFT) . '","' .
+              $cookedMeasurement[3] .
+            '")';
+          }
+          $values = implode(", ", $values);
+
+          $sql = "insert into `$db->MeasurementSearchValues`".
+                  " (item_id, height, width, depth, unit)".
+                  " values $values";
+          $db->query($sql);
+          // die();
+
+        } # if ($cookedMeasuerements)
       } # if ($text)
     } # if ($item_id)
   } #  _preProcessItem()
+
+  # ----------------------------------------------------------------------------
 
   /**
 	 * Main regex processing to extract measurements, to be able to store them later
@@ -400,7 +435,9 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 		# echo "<pre>" . print_r($cookedMeasuerements,true) . "</pre>"; die();
 
 		return $cookedMeasuerements;
-	}
+	} # _processMeasurementText()
+
+  # ----------------------------------------------------------------------------
 
   /**
 	 * Create the necessary regEx expressions to deal with 123x456x789 measurements
@@ -415,10 +452,13 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
     $DBunits = SELF::_decodeUnitsForRegEx();
     if ($DBunits) { $fullRegEx .= "\s(" . implode("|", $DBunits) . ")"; }
 
+    $oneOrTwoNumbers = "$oneNumber(?:\s*-\s*$oneNumber)?";
+
     $result = array(
       "oneNumber" => $oneNumber,
       "mainTriple" => $mainTriple,
       "fullRegEx" => $fullRegEx,
+      "oneOrTwoNumbers" => $oneOrTwoNumbers,
     );
 
     // echo "<pre>\n\n\n\n\n</pre>";
@@ -426,5 +466,127 @@ class MeasurementSearchPlugin extends Omeka_Plugin_AbstractPlugin {
 
     return $result;
   }
+
+  # ----------------------------------------------------------------------------
+
+  /**
+	 * Display the time search form on the admin advanced search page
+	 */
+	protected function _itemsSearch() {
+		$validUnits = SELF::_decodeUnitsForRegEx();
+		if ($validUnits) {
+			echo common('measurement-search-advanced-search', array("selectUnits" => $validUnits ));
+		}
+	} # _itemsSearch()
+
+	/**
+	 * Display the time search form on the admin advanced search page in admin
+	 */
+	public function hookAdminItemsSearch() { SELF::_itemsSearch();  }
+
+	/**
+	 * Display the time search form on the admin advanced search page in admin
+	 */
+	public function hookPublicItemsSearch() { SELF::_itemsSearch();  }
+
+  /**
+  * Debug output of stored measurement in item's sidebar (if activated)
+  *
+  * @param Item $item
+  */
+  public function hookAdminItemsShowSidebar($args) {
+		$debugOutput = (int)(boolean) get_option('measurement_search_debug_output');
+		if ($debugOutput) {
+			$itemID = $args['item']['id'];
+			if ($itemID) {
+				echo "<div class='panel'><h4>".__("Measurement Search Debug Output")."</h4>\n";
+				$db = get_db();
+				$sql = "select * from `$db->MeasurementSearchValues` where item_id=$itemID";
+				$measurements = $db->fetchAll($sql);
+				if ($measurements) {
+					echo "<ul>\n";
+					foreach($measurements as $measurement) {
+						echo "<li>".
+                    ltrim($measurement["height"], '0'). "x" .
+                    ltrim($measurement["width"], '0'). "x" .
+                    ltrim($measurement["depth"], '0'). " " .
+                    $measurement["unit"].
+									"</li>\n";
+					}
+					echo "</ul>\n";
+				}
+				echo "</div>\n";
+			}
+		} # if ($debugOutput)
+	} # hookAdminItemsShowSidebar()
+
+  # ----------------------------------------------------------------------------
+
+  /**
+	 * Filter for a measurement after search page submission.
+	 *
+	 * @param array $args
+	 */
+	public function hookItemsBrowseSql($args) {
+		$select = $args['select'];
+		$params = $args['params'];
+
+		$regEx = SELF::_constructRegEx();
+		foreach($regEx as $key => $val) { $$key = $val; }
+
+		if (	(isset($params['measurement_search_term'])) and
+					(preg_match( "($oneOrTwoNumbers)", $params['measurement_search_term'])) ) {
+
+      # echo "<pre>" . $params['measurement_search_term'] . " / " . $oneOrTwoNumbers . "</pre>";
+
+      $singleCount = preg_match_all ( "($oneNumber)", $params['measurement_search_term'], $singleNumbers );
+
+      # echo "<pre>$singleCount: " . print_r($singleNumbers,true) . "</pre>";
+
+      $lower = $singleNumbers[0][0];
+      $higher = ( isset($singleNumbers[0][1]) ? $singleNumbers[0][1] : $lower );
+
+      $lower = str_pad($lower, MEASUREMENTSEARCH_NUM_MAXLEN, "0", STR_PAD_LEFT);
+      $higher = str_pad($higher, MEASUREMENTSEARCH_NUM_MAXLEN, "0", STR_PAD_LEFT);
+
+      # echo "<pre>$lower / $higher</pre>";
+
+      /* * /
+			$db = get_db();
+			$select
+					->join(
+							array('measurement_search_values' => $db->MeasurementSearchValues),
+							"measurement_search_values.item_id = items.id",
+							array()
+					)
+          # +#+#+# ... build intersection through "where" clause
+					->where("'$searchFromNum'<=measurement_search_values.tonum and '$searchToNum'>=measurement_search_values.fromnum");
+
+			if ( (isset($params['measurement_search_unit'])) and (is_array($params['measurement_search_unit'])) ) {
+				$measurementSearchFormUnits = array();
+				foreach($params['measurement_search_unit'] as $unit) { $measurementSearchFormUnits[] = intval($unit); }
+				if ($measurementSearchFormUnits) {
+					$validUnits = SELF::_decodeUnitsForRegEx();
+					if ($validUnits) {
+						$MeasurementSearchUnits = array_keys($validUnits);
+						$dbUnits = array();
+						foreach($measurementSearchFormUnits as $unit) {
+							if (isset($MeasurementSearchUnits[$unit])) { $dbUnits[] = addslashes(MmeasurementSearchUnits[$unit]); }
+						}
+						if ($dbUnits) {
+							$dbUnits = "'" . implode("','", $dbUnits) . "'";
+							$select->where("measurement_search_values.unit in ($dbUnits)");
+						}
+					}
+				}
+			}
+
+			# die("<pre>$searchFromNum / $searchToNum --- $select</pre>");
+
+      /* */
+		}
+	} # hookItemsBrowseSql()
+
+  # ----------------------------------------------------------------------------
 
 } # class
