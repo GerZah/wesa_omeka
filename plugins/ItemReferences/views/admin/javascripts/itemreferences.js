f37a86be2f54b09450ccfe9432a1d2190f094cae
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2016-01-06 18:30:56 +0100

    Removing redundant lity library (redundant/obsolete when re-using ItemRelations selector) / adding hide/show code for unnecessary UI elements

--- plugins/ItemReferences/views/admin/javascripts/itemreferences.js
+++ plugins/ItemReferences/views/admin/javascripts/itemreferences.js
@@ -9,10 +9,16 @@ jQuery(document).bind("omeka:elementformload", function() {
                   "<button class='itemReferencesBtn' data-caltype='' >"+buttonSelect+"</button>"+
             "</span>");
 
-
+  var formerLinkText = "";
 
   $(".itemReferencesBtn").click(function(e) {
     e.preventDefault();
+
+    $("#new_relation_property_id").hide().prev().hide().prev().hide();
+    $("#relation_comment").parent().hide();
+    formerLinkText = $("#add-relation").text();
+    $("#add-relation").text("Select");
+
     lightbox("#item-relation-selector");
   });
 
@@ -21,6 +27,9 @@ jQuery(document).bind("omeka:elementformload", function() {
 
 
   $(document).on('lity:close', function(event, lightbox) {
+    $("#new_relation_property_id").show().prev().show().prev().show();
+    $("#relation_comment").parent().show();
+    $("#add-relation").text(formerLinkText);
   });
 
 
