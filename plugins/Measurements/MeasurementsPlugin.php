733e16f8727b563e730cdcabbd95be4ece33f84e
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2016-02-29 15:29:37 +0100

    Measurements: Reactivated disabled Display filter (d'oh!) (was: Got rid of triple unit index (which could change over time), now storing verbatim triple unit text)

--- plugins/Measurements/MeasurementsPlugin.php
+++ plugins/Measurements/MeasurementsPlugin.php
@@ -48,7 +48,7 @@ class MeasurementsPlugin extends Omeka_Plugin_AbstractPlugin {
 
     // Add filters
     $filter_names = array(
-        // 'Display',
+        'Display',
         'ElementInput',
     );
 
