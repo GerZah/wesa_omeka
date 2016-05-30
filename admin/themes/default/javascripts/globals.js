5ca0fd7416821a08a79cd0280b86550597d419fa
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2016-05-30 11:16:34 +0200

    Master: Update Omeka 2.4 -> 2.4.1

--- admin/themes/default/javascripts/globals.js
+++ admin/themes/default/javascripts/globals.js
@@ -26,7 +26,8 @@ if (!Omeka) {
             plugins: "paste,inlinepopups,media,autoresize",
             media_strict: false,
             width: "100%",
-            autoresize_max_height: 500
+            autoresize_max_height: 500,
+            entities: "160,nbsp,173,shy,8194,ensp,8195,emsp,8201,thinsp,8204,zwnj,8205,zwj,8206,lrm,8207,rlm"
         };
 
         tinyMCE.init($.extend(initParams, params));
