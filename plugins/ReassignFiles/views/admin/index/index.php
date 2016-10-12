537ec47de9e371fe2a1706db9cc9683d24f6df47
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2016-10-12 18:32:50 +0200

    ReassignFiles: Greatly improve performance / scalability / +#+#+# i18n incomplete

--- plugins/ReassignFiles/views/admin/index/index.php
+++ plugins/ReassignFiles/views/admin/index/index.php
@@ -3,6 +3,8 @@ queue_js_file('items');
 queue_js_file('tabs');
 queue_css_file('reassignfiles');
 echo head(array('title' => __('Reassign Files to Item'), 'bodyclass' => 'reassignfiles'));
+$numLatest = ( isset($_GET["numlatest"]) ? intval($_GET["numlatest"]) : 50 );
+$numLatest = ( $numLatest < 50 ? 50 : $numLatest );
 ?>
 <?php echo flash(); ?>
 <div class="drawer-contents">
@@ -11,25 +13,39 @@ echo head(array('title' => __('Reassign Files to Item'), 'bodyclass' => 'reassig
       <h2><?php echo __("Step 1: Select Item"); ?></h2>
       <div class="field">
         <p><?php echo __("Please select an existing item to reassign files to."); ?></p>
+        <p>
+        <?php
+          $extension = 50;
+          $newUrl = $_SERVER['REDIRECT_URL'] . "?numlatest=" . ($numLatest + $extension);
+          echo
+            sprintf(__("<em>Please note:</em> Currently displaying %d latest modified items."), $numLatest)
+            . " <a href='$newUrl'>"
+            . "[" . sprintf(__("Click here to display %d more."), $extension) . "]"
+            . "</a>"
+          ;
+        ?>
+        </p>
       </div>
       <div class="inputs three columns omega">
         <?php
-        $itemNames = array();
-        $sqlDb = get_db();
-        $query = "SELECT record_id, text from {$sqlDb->ElementText} WHERE element_id = 50 GROUP by record_id";
-        $query = "SELECT id as record_id,
-                    ( SELECT text from {$sqlDb->ElementText} et
-                      WHERE element_id = 50
-                      AND et.record_id = it.id
-                      GROUP by et.record_id
-                    ) as text
-                  FROM {$sqlDb->Items} it";
-        $itemNames = $sqlDb->fetchAll($query);
-        $item = array(-1 => __('Select Below'));
-        foreach ($itemNames as $itemName) {
-          $item[$itemName['record_id']] = ( $itemName['text'] ? $itemName['text'] : "[".__("Untitled Item")."]" );
-        }
-        echo $this->formSelect('reassignFilesItem', $item, array('multiple' => false), $item); ?>
+          $itemNames = array();
+          $sqlDb = get_db();
+          $item = array(-1 => __('Select Below'));
+          $query = "
+            SELECT id
+            FROM `$sqlDb->Items`
+            ORDER BY modified DESC
+            LIMIT $numLatest
+          ";
+          $itemIds = $sqlDb->fetchAll($query);
+          foreach ($itemIds as $itemId) {
+            $curItem = get_record_by_id('Item', $itemId["id"]);
+            $item[$itemId["id"]] = metadata($curItem, array('Dublin Core', 'Title')) . " [#".$itemId["id"]."]";
+          }
+          echo $this->formSelect('reassignFilesItem', $item, array('multiple' => false), $item);
+        ?>
+      </div>
+      <div>
       </div>
     </fieldset>
     <fieldset class="bulk-metadata-editor-fieldset" id='bulk-metadata-editor-fields-set' style="border: 1px solid black; padding:15px; margin:10px;">
