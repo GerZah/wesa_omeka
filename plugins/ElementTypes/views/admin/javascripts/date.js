e79e56e33d1e8e1a4a1569ab86e0223087ae5236
Gero Zahn <gerozahn@campus.uni-paderborn.de>
2017-02-02 13:05:40 +0100

    UPDATE to Omeka 2.5

--- plugins/ElementTypes/views/admin/javascripts/date.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).bind("omeka:elementformload", function() {
-    jQuery("input[data-type='date']").each(function() {
-        jQuery(this).datepicker({
-            dateFormat: "yy-mm-dd",
-        });
-    });
-});
